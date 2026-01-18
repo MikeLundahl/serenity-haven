a6ec975e902f25bf484319719ea21ad59d3af579
Mike Lundahl <mikeclundahl@gmail.com>
2026-01-18 19:46:40 +0100

    added wip restriction signs and toggle control and placeholder texture

--- scripts/controls.gd
+++ scripts/controls.gd
@@ -3,8 +3,9 @@ extends Node
 
 # THIS IS GENERIC CONTROLS AND NOT PLAYER CONTROLS
 
-@onready var root_sceen: Node3D = get_parent()
-@onready var animation: AnimationPlayer = root_sceen.get_node("AnimationPlayer")
+@onready var root_scene: Node3D = get_parent()
+@onready var animation: AnimationPlayer = root_scene.get_node("AnimationPlayer")
+@onready var wip_stripes: Node3D = root_scene.get_node_or_null("WIP_stripes")
 
 var is_quitting: bool = false
 
@@ -16,6 +17,10 @@ func _unhandled_input(event: InputEvent) -> void:
 		is_quitting = true
 		if is_quitting:
 			animation.play_backwards("intro")
+			
+	if event.is_action_pressed("wip_restricted_toggle"):
+		if wip_stripes != null:
+			wip_stripes.visible = !wip_stripes.visible
 
 func _on_animation_finished(anim) -> void:
 	if anim == "intro" && is_quitting:
